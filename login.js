82992bab85a28f5ef9c3efc9a9ef8fc815856b3c
yifan-dev <yifan1761@gmail.com>
2026-02-13 06:35:18 +0800

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,19 @@
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword }
+from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 
+const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.onclick = async () => {
+  try {
+    await signInWithEmailAndPassword(
+      auth,
+      email.value,
+      password.value
+    );
+
+    location.href = "admin.html";
+  } catch {
+    alert("登入失敗");
+  }
+};
