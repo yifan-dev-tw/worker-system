1f081c7649af17861a5cbe73255709a523c2afa6
yifan-dev <yifan1761@gmail.com>
2026-02-13 06:18:21 +0800

    Update Firebase SDK to version 12.9.0

--- firebase.js
+++ firebase.js
@@ -1,13 +1,8 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC-ajaVChBSIK7uXIgcv5JTlxDjuxLWHDA",
-  authDomain: "worker-system.firebaseapp.com",
-  projectId: "worker-system",
-  storageBucket: "worker-system.firebasestorage.app",
-  messagingSenderId: "249724369179",
-  appId: "1:249724369179:web:9d14d30c499aec379dbbbe"
+  // 你的設定
 };
 
 const app = initializeApp(firebaseConfig);
