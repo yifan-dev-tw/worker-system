481f5f0ff7153e20cab72cd39a3ee3edd45819bb
yifan-dev <yifan1761@gmail.com>
2026-02-12 13:17:24 +0800

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import { getFirestore } from
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC-ajaVChBSIK7uXIgcv5JTlxDjuxLWHDA",
+  authDomain: "worker-system.firebaseapp.com",
+  projectId: "worker-system",
+  storageBucket: "worker-system.firebasestorage.app",
+  messagingSenderId: "249724369179",
+  appId: "1:249724369179:web:9d14d30c499aec379dbbbe",
+  measurementId: "G-H4WTDJ84TG"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
