e062c09a524d0a1fbb42fa48832bc6ababc554ed
yifan-dev <yifan1761@gmail.com>
2026-02-12 17:33:52 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,5 @@
-import { initializeApp } from
-"https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
-import { getFirestore } from
-"https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC-ajaVChBSIK7uXIgcv5JTlxDjuxLWHDA",
@@ -10,8 +7,7 @@ const firebaseConfig = {
   projectId: "worker-system",
   storageBucket: "worker-system.firebasestorage.app",
   messagingSenderId: "249724369179",
-  appId: "1:249724369179:web:9d14d30c499aec379dbbbe",
-  measurementId: "G-H4WTDJ84TG"
+  appId: "1:249724369179:web:9d14d30c499aec379dbbbe"
 };
 
 const app = initializeApp(firebaseConfig);
