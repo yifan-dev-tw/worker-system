c3a9cb0edb0a1ecc87e42d04cb807fe6baface8e
yifan-dev <yifan1761@gmail.com>
2026-02-13 06:41:11 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 
 const firebaseConfig = {
-  // 你的設定
+  // 你的 Firebase config
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
-export { db };
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
